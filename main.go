eb5778794435bf3ed35539d6306504e046dc1d85
yuanxuan <yuanxuan@didiglobal.com>
2022-03-02 11:16:21 +0800

    new test

---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+* Copyright 2021 xiaojukeji.com Inc. All Rights Reserved.
+*
+* @AUTHOR: Andy Yu<yuanxuan@didiglobal.com>
+* @DATE: 2022/3/2 11:14 上午
+* @FILE: main.go.go
+* @DESC: description of main.go.go
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
